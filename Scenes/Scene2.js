4f357ccc51823d8fbe6eb3bfd56dd682e2b77f59
iandemed <iandemed@gmail.com>
2020-11-23 11:40:45 -0500

    Added animations and now ships explode

--- Scenes/Scene2.js
+++ Scenes/Scene2.js
@@ -4,12 +4,51 @@ class Scene2 extends Phaser.Scene{
     }
 
     create (){
-        this.background = this.add.image(0,0,"background")
+        this.background = this.add.tileSprite(0,0, config.width, config.height, "background")
         this.background.setOrigin(0,0)
 
-        this.ship1 = this.add.image(config.width/2 - 50, config.height/2, "ship")
-        this.ship2 = this.add.image(config.width/2, config.height/2, "ship2")
-        this.ship3 = this.add.image(config.width/2 + 50, config.height/2, "ship3")
+        this.ship1 = this.add.sprite(config.width/2 - 50, config.height/2, "ship")
+        this.ship2 = this.add.sprite(config.width/2, config.height/2, "ship2")
+        this.ship3 = this.add.sprite(config.width/2 + 50, config.height/2, "ship3")
+
+        /* --- Create the animations for the ships --- */
+        this.anims.create({
+            key: "ship1_anim",
+            frames: this.anims.generateFrameNumbers("ship"),
+            frameRate: 20,
+            repeat: -1 // infite loop
+        })
+        this.anims.create({
+            key: "ship2_anim",
+            frames: this.anims.generateFrameNumbers("ship2"),
+            frameRate: 20,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "ship3_anim",
+            frames: this.anims.generateFrameNumbers("ship3"),
+            frameRate: 20,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "explode",
+            frames: this.anims.generateFrameNumbers("explosion"),
+            frameRate: 20,
+            repeat: 0,
+            hideOnComplete: true
+        })
+        
+        /* --- Play the animations --- */
+        this.ship1.play("ship1_anim")
+        this.ship2.play("ship2_anim")
+        this.ship3.play("ship3_anim")
+
+        this.ship1.setInteractive()
+        this.ship2.setInteractive()
+        this.ship3.setInteractive()
+
+        this.input.on('gameobjectdown', this.destroyShip, this)
+
     }
 
     moveShip(ship, speed){
@@ -22,13 +61,20 @@ class Scene2 extends Phaser.Scene{
 
     update() {
         this.moveShip(this.ship1, 1)
-        this.moveShip(this.ship2, 2)
-        this.moveShip(this.ship3, 3)
+        this.moveShip(this.ship2, 1)
+        this.moveShip(this.ship3, 1)
+
+        this.background.tilePositionY -= 0.5
     }
 
     resetShipPos(ship){
         ship.y = 0
         let randomX = Phaser.Math.Between(0, config.width)
         ship.x = randomX
+    }
+
+    destroyShip(pointer, gameObject){
+        gameObject.setTexture("explosion")
+        gameObject.play("explode")
     }
 }
